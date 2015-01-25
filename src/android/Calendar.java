e3c371e8191d68a6d2fe4bb69f9206484dd3a2fa
Jeremy Plack <stlouisweb@gmail.com>
2015-01-25 14:32:14 -0600

    fixes typo in Calendar.java

--- src/android/Calendar.java
+++ src/android/Calendar.java
@@ -1,4 +1,4 @@
-package com.stlouisweb.calendarplugin;
+package com.stlouisweb.calendar;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
@@ -16,14 +16,14 @@ public class Calendar extends CordovaPlugin {
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         try {
             if (ACTION_ADD_CALENDAR_ENTRY.equals(action)) {
-                    JSONObject arg_object = args.getJSONObject(0);
-                    Intent calIntent = new Intent(Intent.ACTION_EDIT)
-                .setType("vnd.android.cursor.item/event")
-                .putExtra("beginTime", arg_object.getLong("startTimeMillis"))
-                .putExtra("endTime", arg_object.getLong("endTimeMillis"))
-                .putExtra("title", arg_object.getString("title"))
-                .putExtra("description", arg_object.getString("description"))
-                .putExtra("eventLocation", arg_object.getString("eventLocation"));
+                JSONObject arg_object = args.getJSONObject(0);
+                Intent calIntent = new Intent(Intent.ACTION_EDIT)
+                    .setType("vnd.android.cursor.item/event")
+                    .putExtra("beginTime", arg_object.getLong("startTimeMillis"))
+                    .putExtra("endTime", arg_object.getLong("endTimeMillis"))
+                    .putExtra("title", arg_object.getString("title"))
+                    .putExtra("description", arg_object.getString("description"))
+                    .putExtra("eventLocation", arg_object.getString("eventLocation"));
 
                 this.cordova.getActivity().startActivity(calIntent);
                 callbackContext.success();
